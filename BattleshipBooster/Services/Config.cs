bea50afc06dbec4d653b355ed1ca9cbd3be90cfc
Adrian Habeler <adrian.habeler@gmail.com>
2020-12-13 20:29:15 +0100

    material design (so guet wie möglich)

--- BattleshipBooster/Services/Config.cs
+++ BattleshipBooster/Services/Config.cs
@@ -14,8 +14,8 @@ namespace BattleshipBooster.Services
 			return size switch
 			{
 				5 => new PlayFieldConfig(new Boat[] { new Boat(3), new Boat(2), new Boat(1), new Boat(1) }, 1, 2),
-				6 => new PlayFieldConfig(new Boat[] { new Boat(3), new Boat(2), new Boat(2), new Boat(1), new Boat(1) }, 2, 2),
-				7 => new PlayFieldConfig(new Boat[] { new Boat(3), new Boat(2), new Boat(2), new Boat(2), new Boat(1), new Boat(1), new Boat(1) }, 2, 3),
+				6 => new PlayFieldConfig(new Boat[] { new Boat(3), new Boat(2), new Boat(2), new Boat(1), new Boat(1) }, 2, 3),
+				7 => new PlayFieldConfig(new Boat[] { new Boat(3), new Boat(2), new Boat(2), new Boat(2), new Boat(1), new Boat(1), new Boat(1) }, 3, 4),
 				_ => new PlayFieldConfig(new Boat[0], 0, 0),
 			};
 		}
