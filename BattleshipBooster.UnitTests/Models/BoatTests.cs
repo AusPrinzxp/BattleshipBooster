7b6369a25563d8dfa097bdfa0dce54d18630b18a
Silvan <seugster@online.gibz.ch>
2020-12-13 12:54:04 +0100

    added BoatTest function

---
+++ BattleshipBooster.UnitTests/Models/BoatTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BattleshipBooster.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleshipBooster.Models.Tests
+{
+    [TestClass()]
+    public class BoatTests
+    {
+        [TestMethod()]
+        public void PlaceTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
