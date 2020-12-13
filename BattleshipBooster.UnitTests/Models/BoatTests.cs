72eff10377c041916bf93aaeaf3059e106ab49b1
Silvan <seugster@online.gibz.ch>
2020-12-13 20:03:57 +0100

    added BoatTests, GeneratorTests, MainViewModelTests

--- BattleshipBooster.UnitTests/Models/BoatTests.cs
+++ BattleshipBooster.UnitTests/Models/BoatTests.cs
@@ -10,12 +10,12 @@ namespace BattleshipBooster.Models.Tests
     public class BoatTests
     {
         [TestMethod()]
-        public void PlaceTest()
+        public void PlaceTestVertical()
         {
             // Arrange
             int size = 3;
             Boat boat = new Boat(2);
-            StartPosition verticalStartPosition = new StartPosition(0, 0, false);
+            StartPosition startPosition = new StartPosition(0, 0, false);
             Field[,] fields = new Field[size, size];
 
             for (int i = 0; i < size; i++)
@@ -26,10 +26,33 @@ namespace BattleshipBooster.Models.Tests
                 }
             }
             // Act
-            boat.Place(fields, verticalStartPosition);
+            boat.Place(fields, startPosition);
             // Assert
             Assert.AreEqual(fields[0, 0].Icon, "BoatEndUp");
             Assert.AreEqual(fields[0, 1].Icon, "BoatEndDown");
+        }
+
+        [TestMethod()]
+        public void PlaceTestHorizontal()
+        {
+            // Arrange
+            int size = 3;
+            Boat boat = new Boat(2);
+            StartPosition startPosition = new StartPosition(0, 0, true);
+            Field[,] fields = new Field[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    fields[i, j] = new Field("", true, false);
+                }
+            }
+            // Act
+            boat.Place(fields, startPosition);
+            // Assert
+            Assert.AreEqual(fields[0, 0].Icon, "BoatEndLeft");
+            Assert.AreEqual(fields[1, 0].Icon, "BoatEndRight");
         }
     }
 }
